84312d6d539c201c11cb63d3f513c16522008152
MisterSyntax <asyriala@gmail.com>
2017-05-09 19:15:54 -0400

    added some store stuff

--- Notes/Store.js
+++ Notes/Store.js
@@ -1,9 +1,12 @@
 import C from "./constants";
 import appReducer from "./store/reducers";
+import initialState from "./initialState.json";
 import { createStore } from "redux";
 
+//createStore can be passed the reducers, and optionally an initial state
 const store = createStore(appReducer, initialState);
 
+//you can also subscribe to the store and run callbacks when actions are dispatched
 store.subscribe(()=> console.log(store.getState()));
 
 store.dispatch({
@@ -15,3 +18,4 @@ store.dispatch({
     "backcountry": true
   }
 });
+
